a8203b7c0b2e09d22547e637ae6761ac2a386948
diei-pi <jp.chagasrodrigues@gmail.com>
2022-08-10 22:29:55 -0300

    ajustando testes quebrados

--- test_main.py
+++ test_main.py
@@ -34,14 +34,18 @@ def test_deve_calcular_uma_corrida_com_taxa_minima():
     assert fare == 10
 
 def test_nao_deve_calcular_uma_corrida_com_distancia_invalida():
-    ride = Ride(date=datetime(year=2022,month=8,day=9,hour=22),distance=-10)
-    rideArray = [ride]
-    fare = calculate_ride(rideArray)
-    assert fare == -1
+    try:
+        ride = Ride(date=datetime(year=2022,month=8,day=9,hour=22),distance=-10)
+        rideArray = [ride]
+        fare = calculate_ride(rideArray)
+    except Exception as exception:
+        assert str(exception) == "Invalid distance"
 
 def test_nao_deve_calcular_uma_corrida_com_data_invalida():
-    ride = Ride(date=123,distance=10)
-    rideArray = [ride]
-    fare = calculate_ride(rideArray)
-    assert fare == -2
+    try:
+        ride = Ride(date=123,distance=10)
+        rideArray = [ride]
+        fare = calculate_ride(rideArray)
+    except Exception as exception:
+        assert str(exception) == "Invalid date"
 
